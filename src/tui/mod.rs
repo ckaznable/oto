ee54a3be0c345cd8f2fbbb989e8dd2320fd3e028
ckaznable <ckaznable@gmail.com>
2026-01-11 14:27:34 +0800

    feat: impl state bar

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,16 +1,55 @@
 use enclose::enclose;
-use std::sync::mpsc::{Receiver, Sender};
+use strum::Display;
+use std::{
+    cell::Cell,
+    rc::Rc,
+    sync::mpsc::{Receiver, Sender},
+};
 
-use ratatui::{DefaultTerminal, Frame, crossterm};
+use ratatui::{
+    DefaultTerminal, Frame, crossterm::{self, event::Event},
+    layout::{Constraint, Layout},
+};
 
-use crate::event::{AppCommand, PlayerCommand};
+use crate::{event::{AppCommand, PlayerCommand}, tui::state_bar::StateBar};
 
-#[derive(Clone)]
-struct AppStatus {}
+pub mod state_bar;
 
-impl AppStatus {
+#[derive(Clone, Copy, Default, Display)]
+pub enum AppMode {
+    #[default]
+    Normal,
+    Playing,
+    Pause,
+}
+
+#[derive(Clone, Copy, Default)]
+pub enum PlayMode {
+    #[default]
+    Normal,
+    Loop,
+    LoopCurrent,
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct PlayingState {
+    current: usize,
+    duration: usize,
+}
+
+#[derive(Clone, Default)]
+pub struct AppState {
+    app_mode: Rc<Cell<AppMode>>,
+    play_mode: Rc<Cell<PlayMode>>,
+    playing: Rc<Cell<PlayingState>>,
+    volumn: Rc<Cell<u8>>,
+}
+
+impl AppState {
     fn new() -> Self {
-        Self {}
+        Self {
+            ..Default::default()
+        }
     }
 }
 
@@ -25,7 +64,7 @@ pub fn tui(
 }
 
 fn app(terminal: &mut DefaultTerminal, rx: Receiver<AppCommand>) -> std::io::Result<()> {
-    let status = AppStatus::new();
+    let status = AppState::new();
 
     loop {
         terminal.draw(enclose!((status) move |f| render(f, status)))?;
@@ -39,18 +78,29 @@ fn app(terminal: &mut DefaultTerminal, rx: Receiver<AppCommand>) -> std::io::Res
     }
 }
 
-fn render(frame: &mut Frame, status: AppStatus) {
-    frame.render_widget("hello world", frame.area());
+fn render(frame: &mut Frame, mut state: AppState) {
+    let area = frame.area();
+    let layout = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Fill(1),
+        Constraint::Length(1),
+    ]);
+
+    let [top, middle, bottom] = layout.areas(area);
+    frame.render_stateful_widget(StateBar, bottom, &mut state);
 }
 
 fn handle_keypress(tx: Sender<AppCommand>) {
     loop {
         match crossterm::event::read() {
-            Ok(_) => {}
+            Ok(Event::Key(_)) => {
+                tx.send(AppCommand::End).ok();
+            },
             Err(e) => {
                 tx.send(AppCommand::Err("crossterm event reader error occurred"))
                     .ok();
             }
+            _ => {}
         }
     }
 }
---
+++ src/tui/state_bar.rs
@@ -0,0 +1,69 @@
+use ratatui::{
+    layout::{Constraint, Flex, Layout},
+    prelude::*,
+    widgets::StatefulWidget,
+};
+
+use crate::tui::AppState;
+
+pub struct StateBar;
+
+impl StatefulWidget for StateBar {
+    type State = AppState;
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        let app_mode_str = state.app_mode.get().to_string();
+
+        let vol_icon = match state.volumn.get() {
+            0 => "",
+            1..50 => "",
+            _ => "",
+        };
+
+        let vol_str = format!(" {} {}% ", vol_icon, state.volumn.get());
+
+        let layout = Layout::horizontal([
+            Constraint::Length((app_mode_str.len() + 3) as u16),
+            Constraint::Fill(1),
+            Constraint::Length((vol_str.len() + 2) as u16),
+        ]);
+
+        let [left, middle, right] = layout.areas(area);
+
+        let line = Line::from(vec![
+            Span::styled(format!(" {app_mode_str} "), Style::new().bg(Color::Green)),
+            Span::styled("", Style::new().fg(Color::Green)),
+        ]);
+        line.render(left, buf);
+
+        let layout = Layout::horizontal([
+            Constraint::Length(3),
+            Constraint::Length(vol_str.len() as u16),
+        ]);
+        let [left, right] = layout.areas(right);
+
+        let line = Line::from(vec![
+            Span::styled("", Style::new().fg(Color::Blue).bg(Color::Cyan)),
+            Span::styled(vol_str, Style::new().bg(Color::Blue)),
+        ]);
+        line.render(right, buf);
+
+        let line = Line::from(vec![Span::styled(
+            format!(" {} ", ""),
+            Style::new().bg(Color::Cyan),
+        )]);
+        line.render(left, buf);
+
+        let playing = state.playing.get();
+        let duration = format!("{:02}:{:02}", playing.duration / 60, playing.duration % 60);
+        let current = format!("{:02}:{:02}", playing.current / 60, playing.current % 60);
+        let layout = Layout::horizontal([Constraint::Length(11)]).flex(Flex::Center);
+        let [progress] = layout.areas(middle);
+        let line = Line::from(vec![Span::styled(
+            format!("{}/{}", current, duration),
+            Style::default().fg(Color::White),
+        )]);
+
+        line.render(progress, buf);
+    }
+}
